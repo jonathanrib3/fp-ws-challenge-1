212917ec1e3f5d6529bc554ad525dc901862f46a
jonathanrib3 <jonathanrib.3@gmail.com>
2024-10-04 17:20:15 -0300

    refactor: use fastify route adapter with currying)

---
+++ src/fastify-route-adapter.js
@@ -0,0 +1,17 @@
+import getOperationFromUrl from "./getOperationFromUrl.js"
+
+export const adaptRoute = (controller) => {
+  return (req, res) => {
+    const num1 = Number(req.query.num1);
+    const num2 = Number(req.query.num2);
+    const operationRequest = {
+      num1,
+      num2,
+      operation: getOperationFromUrl(req.originalUrl)
+    };
+
+    const result = controller(operationRequest);
+
+    res.send(result);
+  }
+}
