05e04c99c6e86d872fac6d1e39a66ead28e336cd
jonathanrib3 <jonathanrib.3@gmail.com>
2024-10-08 17:10:38 -0300

    refactor: separate express and fastify servers configurations into modules

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
-import express from "express";
-import router from "./src/expressRoutes.js";
-import Fastify from "fastify";
-import fastifyRoutes from "./src/fastifyRoutes.js";
+import { expressApp } from "./src/expressApp.js";
+import { fastifyApp } from "./src/fastifyApp.js";
 
-const expressApp = express();
 const expressPort = 3000;
-const fastify = Fastify();
 const fastifyPort = 3001;
-
-expressApp.use(router);
-fastify.register(fastifyRoutes);
 
 expressApp.listen(expressPort, () => {
   console.log('Express server running on port ', expressPort);
 });
 
-fastify.listen({ port: fastifyPort }, (err, address) => {
+fastifyApp.listen({ port: fastifyPort }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
   }
   console.log(`Fastify server listening at ${address}`);
-})
+});
